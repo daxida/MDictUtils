17c0f9c158783a41088d16ad31ed42633c3f6496
stephenmk <stephenmk@users.noreply.github.com>
2026-04-10 22:35:14 -0500

    Reimplement `MDictKeyComparer.Compare` method
    Explanation and benchmarks in comments of #2

--- Lib/Lib.cs
+++ Lib/Lib.cs
@@ -694,31 +694,56 @@ internal partial class MDictKeyComparer
     [GeneratedRegex(@"[!\""#$%&'()*+,\-./:;<=>?@\[\\\]^_`{|}~ ]+")]
     public static partial Regex RegexStrip { get; }
 
-    public static int Compare(string key1, string key2, bool isMdd)
+    public static int Compare(ReadOnlySpan<char> k1, ReadOnlySpan<char> k2, bool isMdd)
     {
-        string k1 = key1.ToLower();
-        string k2 = key2.ToLower();
-
         if (!isMdd)
         {
-            k1 = RegexStrip.Replace(k1, "");
-            k2 = RegexStrip.Replace(k2, "");
+            if (RegexStrip.IsMatch(k1))
+                k1 = StripPunctuation(k1);
+
+            if (RegexStrip.IsMatch(k2))
+                k2 = StripPunctuation(k2);
         }
 
         // key1 = locale.strxfrm(key1) ??
         // this was locale dependent in py, but then we don't pass our tests,
         // and it shouldn't matter anyway as long as the internal mapping works
-        int cmp = string.CompareOrdinal(k1, k2);
-        if (cmp != 0) return cmp;
+        int cmp = k1.CompareTo(k2, StringComparison.OrdinalIgnoreCase);
+
+        if (cmp != 0)
+            return cmp;
 
         // reverse length (longer first) - compare on current k1/k2
         if (k1.Length != k2.Length)
             return k2.Length.CompareTo(k1.Length);
 
-        // strip punctuation
-        k1 = k1.TrimEnd(PunctuationChars);
-        k2 = k2.TrimEnd(PunctuationChars);
+        // trim punctuation (already stripped if this is not MDD)
+        if (isMdd)
+        {
+            k1 = k1.TrimEnd(PunctuationChars);
+            k2 = k2.TrimEnd(PunctuationChars);
+        }
 
-        return string.CompareOrdinal(k2, k1);
+        return k2.CompareTo(k1, StringComparison.OrdinalIgnoreCase);
+    }
+
+    private static ReadOnlySpan<char> StripPunctuation(ReadOnlySpan<char> text)
+    {
+        Span<char> buffer = new char[text.Length];
+
+        int lastIndex = 0;
+        int charsWritten = 0;
+
+        foreach (var match in RegexStrip.EnumerateMatches(text))
+        {
+            text[lastIndex..match.Index].CopyTo(buffer[charsWritten..]);
+            charsWritten += match.Index - lastIndex;
+            lastIndex = match.Index + match.Length;
+        }
+
+        text[lastIndex..text.Length].CopyTo(buffer[charsWritten..]);
+        charsWritten += text.Length - lastIndex;
+
+        return buffer[..charsWritten];
     }
 }
