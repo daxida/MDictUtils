6b6b53aa7d57b614821210915e1ff07cba5be0a6
stephenmk <stephenmk@users.noreply.github.com>
2026-04-13 03:05:37 -0500

    Add `ALLOW_UNSAFE_BLOCKS` constant to .csproj
    The option can be toggled at the command line via a parameter.
    
    `dotnet run -p:AllowUnsafeBlocks=true`
    `dotnet run -p:AllowUnsafeBlocks=false`

--- Lib/ZLibCompression.cs
+++ Lib/ZLibCompression.cs
@@ -1,31 +1,33 @@
 using System;
 using System.IO;
 using System.IO.Compression;
+
+#if ALLOW_UNSAFE_BLOCKS
 using System.Runtime.InteropServices;
+#endif
 
 namespace Lib;
 
 internal static class ZLibCompression
 {
-    public static void Decompress(ReadOnlySpan<byte> input, Span<byte> output)
+
+#if ALLOW_UNSAFE_BLOCKS
+
+    public static unsafe int Compress(ReadOnlySpan<byte> input, Span<byte> output)
     {
-        // The .ToArray() allocation here is unfortunately unavoidable.
-        // See: https://github.com/dotnet/runtime/issues/24622
-        // Unless we want to enable the "unsafe" compiler flag.
-        // See: https://stackoverflow.com/a/48223990
-        using var ms = new MemoryStream(input.ToArray());
-        using var z = new ZLibStream(ms, CompressionMode.Decompress);
-
-        z.ReadExactly(output);
-
-        if (z.ReadByte() is not -1)
+        Console.Error.WriteLine("Hello unsafe!");
+        fixed (byte* pBuffer = &MemoryMarshal.GetReference(output))
         {
-            throw new OverflowException($"More than expected {output.Length} bytes in decompression stream");
+            using var ms = new UnmanagedMemoryStream(pBuffer, output.Length, output.Length, FileAccess.Write);
+            using (var z = new ZLibStream(ms, CompressionLevel.Optimal, leaveOpen: true))
+            {
+                z.Write(input);
+            }
+            return Convert.ToInt32(ms.Length);
         }
     }
 
-    #if false
-    public static unsafe void DecompressUnsafe(ReadOnlySpan<byte> input, Span<byte> output)
+    public static unsafe void Decompress(ReadOnlySpan<byte> input, Span<byte> output)
     {
         fixed (byte* pBuffer = &MemoryMarshal.GetReference(input))
         {
@@ -40,17 +42,17 @@ internal static class ZLibCompression
             }
         }
     }
-    #endif
 
-    /// <remarks>
+#else
+
     /// python default is -1 == 6 , see: https://docs.python.org/3/library/zlib.html#zlib.Z_DEFAULT_COMPRESSION
     /// c# are cooked, custom-made levels, and may not correspond to anything
     /// https://learn.microsoft.com/en-us/dotnet/api/system.io.compression.compressionlevel?view=net-10.0
     ///
     /// There is no reliable way to get the same exact bytes, so live with that
-    /// </remarks>
     public static int Compress(ReadOnlySpan<byte> input, byte[] output)
     {
+        Console.Error.WriteLine("Hello safe!");
         using var ms = new MemoryStream(output);
         using (var z = new ZLibStream(ms, CompressionLevel.Optimal, leaveOpen: true))
         {
@@ -59,18 +61,23 @@ internal static class ZLibCompression
         return Convert.ToInt32(ms.Length);
     }
 
-    #if false
-    public static unsafe int CompressUnsafe(ReadOnlySpan<byte> input, Span<byte> output)
+    /// The .ToArray() allocation here is unfortunately unavoidable.
+    /// See: https://github.com/dotnet/runtime/issues/24622
+    /// Unless we want to enable the "unsafe" compiler flag.
+    /// See: https://stackoverflow.com/a/48223990
+    public static void Decompress(ReadOnlySpan<byte> input, Span<byte> output)
     {
-        fixed (byte* pBuffer = &MemoryMarshal.GetReference(output))
+        using var ms = new MemoryStream(input.ToArray());
+        using var z = new ZLibStream(ms, CompressionMode.Decompress);
+
+        z.ReadExactly(output);
+
+        if (z.ReadByte() is not -1)
         {
-            using var ms = new UnmanagedMemoryStream(pBuffer, output.Length, output.Length, FileAccess.Write);
-            using (var z = new ZLibStream(ms, CompressionLevel.Optimal, leaveOpen: true))
-            {
-                z.Write(input);
-            }
-            return Convert.ToInt32(ms.Length);
+            throw new OverflowException($"More than expected {output.Length} bytes in decompression stream");
         }
     }
-    #endif
+
+#endif
+
 }
