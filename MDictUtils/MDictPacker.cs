daxida/MDictUtils

commit 1512c90d624fd3662dd655c3171258182d42e196

Put a limit of 2GB on invididual record sizes
Hope nobody wants to add HD video to their dictionary?

MDictUtils/MDictPacker.cs
@@ -10,6 +10,7 @@ public static class MDictPacker
     // python does not include the BOM in the title/description
     // so we do the same to allow for oracle testing (but it should not matter really)
     private static readonly UTF8Encoding UTF8NoBOM = new(false);
+    private const long MaxRecordSize = 2_000_000_000;
 
     public static void Unpack(string target, string source, bool isMdd)
     {
@@ -122,7 +123,11 @@ public static class MDictPacker
         if (File.Exists(source))
         {
             // Single file (wtf is happening with separators?)
-            long size = new FileInfo(source).Length;
+            var info = new FileInfo(source);
+            int size = info.Length < MaxRecordSize
+                ? Convert.ToInt32(info.Length)
+                : throw new InvalidDataException($"File '{info.FullName}' is too large (over {MaxRecordSize:N0} bytes)");
+
             string key = "\\" + Path.GetFileName(source);
             if (Path.DirectorySeparatorChar != '\\')
                 key = key.Replace(Path.DirectorySeparatorChar, '\\');
@@ -135,9 +140,14 @@ public static class MDictPacker
             string relpath = source;
             foreach (var fpath in Directory.GetFiles(source, "*", SearchOption.AllDirectories))
             {
+                var info = new FileInfo(source);
+
                 /// TODO: An error will be thrown later if this length is equal to zero.
                 /// <see cref="Build.Blocks.MddRecordBlocksBuilder.WriteBytes"/>
-                long size = new FileInfo(fpath).Length;
+                int size = info.Length < MaxRecordSize
+                    ? Convert.ToInt32(info.Length)
+                    : throw new InvalidDataException($"File '{info.FullName}' is too large (over {MaxRecordSize:N0} bytes)");
+
                 string key = "\\" + Path.GetRelativePath(relpath, fpath);
                 if (Path.DirectorySeparatorChar != '\\')
                     key = key.Replace(Path.DirectorySeparatorChar, '\\');
@@ -201,7 +211,11 @@ public static class MDictPacker
                     if (key == null || offset == pos)
                         throw new Exception($"Error at line {lineNum}: {path}");
 
-                    long size = offset - pos + nullLength;
+                    long longSize = offset - pos + nullLength;
+                    int size = longSize < MaxRecordSize
+                        ? Convert.ToInt32(longSize)
+                        : throw new InvalidDataException($"File '{path}' contains a record that is too large (over {MaxRecordSize:N0} bytes)");
+
                     entries.Add(new MDictEntry(key, pos, path, size));
                     key = null;
                 }

MDictUtils/Build/Blocks/BlocksBuilder.cs
@@ -18,7 +18,7 @@ internal abstract partial class BlocksBuilder<T>
     private readonly static string _typeName = typeof(T).Name;
 
     protected abstract T BlockConstructor(ReadOnlySpan<OffsetTableEntry> entries);
-    protected abstract long GetByteCount(OffsetTableEntry entry);
+    protected abstract int GetByteCount(OffsetTableEntry entry);
     protected abstract int WriteBytes(OffsetTableEntry entry, Span<byte> buffer);
 
     protected ImmutableArray<T> BuildBlocks(OffsetTable offsetTable, int desiredBlockSize)
@@ -82,7 +82,7 @@ internal abstract partial class BlocksBuilder<T>
 
     protected CompressedBlock GetCompressedBlock(ReadOnlySpan<OffsetTableEntry> entries)
     {
-        int totalSize = Convert.ToInt32(entries.Sum(GetByteCount));
+        int totalSize = entries.Sum(GetByteCount);
         var uncompressed = _arrayPool.Rent(totalSize);
 
         int position = 0;

MDictUtils/Build/Blocks/KeyBlocksBuilder.cs
@@ -19,7 +19,7 @@ internal sealed class KeyBlocksBuilder
         return new(block, entries);
     }
 
-    protected override long GetByteCount(OffsetTableEntry entry)
+    protected override int GetByteCount(OffsetTableEntry entry)
         => entry.KeyDataSize;
 
     protected override int WriteBytes(OffsetTableEntry entry, Span<byte> buffer)

MDictUtils/Build/Blocks/MdxRecordBlocksBuilder.cs
@@ -22,7 +22,7 @@ internal sealed class MdxRecordBlocksBuilder
 
     protected override int WriteBytes(OffsetTableEntry entry, Span<byte> buffer)
     {
-        int size = Convert.ToInt32(GetByteCount(entry));
+        int size = GetByteCount(entry);
         if (size < 1)
             throw new InvalidDataException("Size must be >= 1");
 

MDictUtils/Build/Blocks/RecordBlocksBuilder.cs
@@ -12,7 +12,7 @@ internal abstract class RecordBlocksBuilder
 {
     public abstract ImmutableArray<RecordBlock> Build(OffsetTable offsetTable, int desiredBlockSize);
 
-    protected sealed override long GetByteCount(OffsetTableEntry entry)
+    protected sealed override int GetByteCount(OffsetTableEntry entry)
         => entry.RecordSize;
 
     protected sealed override RecordBlock BlockConstructor(ReadOnlySpan<OffsetTableEntry> entries)

MDictUtils/BuildModels/MDictData.cs
@@ -30,7 +30,6 @@ internal readonly record struct CompressedBlock(ImmutableArray<byte> Bytes, long
 internal readonly record struct OffsetTable(ImmutableArray<OffsetTableEntry> Entries)
 {
     public int Length => Entries.Length;
-    public long TotalRecordLength => Entries.Sum(static e => e.RecordSize);
     public ReadOnlySpan<OffsetTableEntry> AsSpan(Range range) => Entries.AsSpan(range);
     public Dictionary<string, int> GetFilePathToTotalEntryCount()
     {

MDictUtils/BuildModels/OffsetTableEntry.cs
@@ -27,7 +27,7 @@ internal sealed class OffsetTableEntry
     /// <summary>
     /// Size of this entry's data in the file from which it is sourced.
     /// </summary>
-    public required long RecordSize { get; init; }
+    public required int RecordSize { get; init; }
 
     /// <summary>
     /// Position of this entry's data in the file from which this entry is sourced.

MDictUtils/MDictWriter.cs
@@ -4,7 +4,7 @@ using MDictUtils.BuildModels;
 
 namespace MDictUtils;
 
-public sealed record MDictEntry(string Key, long Pos, string Path, long Size)
+public sealed record MDictEntry(string Key, long Pos, string Path, int Size)
 {
     public override string ToString()
         => $"Key=\"{Key}\", Pos={Pos}, Size={Size}";

commit 655918eb9dc1437a81265a1edcf19073475d70fc

Fix bug that was introduced in commit 1512c90

MDictUtils/MDictPacker.cs
@@ -140,7 +140,7 @@ public static class MDictPacker
             string relpath = source;
             foreach (var fpath in Directory.GetFiles(source, "*", SearchOption.AllDirectories))
             {
-                var info = new FileInfo(source);
+                var info = new FileInfo(fpath);
 
                 /// TODO: An error will be thrown later if this length is equal to zero.
                 /// <see cref="Build.Blocks.MddRecordBlocksBuilder.WriteBytes"/>